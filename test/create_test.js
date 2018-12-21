acbd5964553285ccbc899930d7c83dff456de18f
unknown <hemapriyasampath@gmail.com>
2018-12-21 06:38:44 -0500

    callback and isNew property

--- test/create_test.js
+++ test/create_test.js
@@ -3,10 +3,16 @@ const users = require('../src/user');
 
 
 describe('Create Users', () =>{
-    it('save the Users',()=>{
+    it('save the Users',(done)=>{
         const Hv = new users({name : 'Hv'});
        
-        Hv.save();
+        Hv.save()
+            .then(() =>{
+                assert(!Hv.isNew);
+                done();
+            })
+        
+
 
     });
 });
--- test/test_helper.js
+++ test/test_helper.js
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose');//
+const mongoose = require('mongoose');
+
 
 mongoose.connect('mongodb://localhost/Users_test');
 mongoose.connection
     .once('open', () => console.log('Good to go!'))
     .on ('error' ,(error)=>{
         console.warn('Warning',error);
+    });
+
+    beforeEach((done)=>{
+        mongoose.connection.collections.users.drop(() =>{
+            done();
+        });
     });
 
 
