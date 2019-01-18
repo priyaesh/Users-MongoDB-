b1ceb552593d184bd5f265bef0a1f5cde376b581
unknown <hemapriyasampath@gmail.com>
2019-01-18 15:32:39 -0500

    Nested assiocation

--- test/assiocation_test.js
+++ test/assiocation_test.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const assert = require('assert');
 const user = require('../src/user');
 const Comment = require('../src/comment');
 const BlogPost = require('../src/blogPost');
@@ -20,13 +21,39 @@ describe('assiocation',() =>{
             Promise.all([joe.save(),myBlogPost.save(),comment.save()])
                 .then(() =>done());
         });   
-        it.only('saves a relation between a user and a blogPost',(done)=>{
+        it('saves a relation between a user and a blogPost',(done)=>{
             user.findOne({name:'joe'})
                 .populate('blogPosts')
                 .then((user)=>{
-                    console.log(user);
+                    assert(user.blogPosts[0].title==='Js is great')
+                    console.log(user.blogPosts[0]);
                     done();
                 });
             });
-        });
+        it.only('saves full relation graph',(done)=>{
+            user.findOne({name:'joe'})
+                .populate({
+                    path: 'blogPosts',
+                    populate:{
+                        path:'comments',
+                        model:'comment',
+                     populate:{
+                        path: 'user',
+                        model:'user'
+                    }
+                }   
+                })
+                .then((user)=>{
+                    console.log(user.blogPosts[0].comments[0].user.name);
+                    assert(user.name ==='joe');
+                    assert(user.blogPosts[0].title ==='Js is great');
+                    assert(user.blogPosts[0].comments[0].content ==='it is my content');
+                    assert(user.blogPosts[0].comments[0].user.name==='joe');
+                    //assert(user.blogPosts[0].)
+                    done();
+                })
+
+                });
+            });
+       
 
